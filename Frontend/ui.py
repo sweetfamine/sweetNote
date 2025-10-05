d67ccf960d96c04eb9c1433a00cca171426bff33
sweetfamine <186402465+sweetfamine@users.noreply.github.com>
2025-10-05 22:30:32 +0200

    added a signature

--- Frontend/ui.py
+++ Frontend/ui.py
@@ -1,6 +1,23 @@
+# ===========================================================
+# Author: sweet.Famine
+# Project: Customer Management System
+# Version: 1.0.0
+# Date: 05.10.2025 22:30Uhr
+#
+# A smal and simple customer management system
+# using customtkinter for GUI and sqlite3 for the database.
+#
+# All rights reserved.
+# see LICENSE for details.
+#
+#    /\_/\
+#   ( o.o )
+#    > ^ <
+# @sweetfamine 2025
+# ===========================================================
+
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
 import customtkinter as ctk
 from Utils.config import Config
 from Manager.customer_manager import CustomerManager
